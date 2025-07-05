9ccf2ba29e0a8ff6e13697abff77dc0e00a88c93
GooberHeads <174388387+GooberHeads6297@users.noreply.github.com>
2025-07-04 19:35:59 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace XboxWebApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a <see cref="Frame">.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TryEnterFullScreenMode();
+
+            Windows.UI.ViewManagement.ApplicationView.PreferredLaunchWindowingMode =
+                Windows.UI.ViewManagement.ApplicationViewWindowingMode.FullScreen;
+
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().SetDesiredBoundsMode(
+                Windows.UI.ViewManagement.ApplicationViewBoundsMode.UseCoreWindow);
+        }
+
+
+    }
+}
